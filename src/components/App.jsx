4a246397606ee2de5745d5c7c488941a23fcf0cd
axtellion <slark19735@gmail.com>
2022-07-12 14:15:01 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,35 +7,23 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    this.setState({ contacts: parsedContacts });
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
 
   addContacts = ({ name, number }) => {
     const errorName = this.state.contacts.find(
       contact => contact.name === name
     );
     if (errorName) {
-      toast.error('This contact is already added');
+      alert('This contact is already added');
       return;
     }
 
@@ -57,14 +45,16 @@ export class App extends Component {
   };
 
   changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+    this.setState({ filter: e.target.value });
   };
 
   getVisibleContact = () => {
     const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLocaleLowerCase();
+
+    const normalizedFilter = filter.toLowerCase();
+
     return contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
@@ -82,7 +72,6 @@ export class App extends Component {
           contacts={visibleTodos}
           onDeleteContact={this.deleteContacts}
         />
-        <ToastContainer theme="colored" autoClose={3000} />
       </Box>
     );
   }
